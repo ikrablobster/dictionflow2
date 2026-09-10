52b7b3e9f20718180e00015fc8d8613db49137dd
Dmytro <149416064+ikrablobster@users.noreply.github.com>
2026-09-11 01:19:36 +0300

    feat: route overlay webview

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Overlay from "./components/Overlay";
 import "./styles/global.css";
 
+const params = new URLSearchParams(window.location.search);
+const Root = params.get("view") === "overlay" ? Overlay : App;
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.StrictMode><Root /></React.StrictMode>
 );
